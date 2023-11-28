54810832f3893601f726c35282517632e4f72c51
Ata Sahin <atasahin05@gmail.com>
2023-11-28 19:00:12 +0300

    bug fix

--- js/landingPageScript.js
+++ js/landingPageScript.js
@@ -6,3 +6,11 @@ function scrollDown() {
 function goToPage(page) {
   window.location.href = page;
 }
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</link>;
